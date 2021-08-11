148dcab5fc9b60b0dc0750742b40f5898ba9639d
matsui54 <haru.matu9168@gmail.com>
2021-08-11 13:59:24 +0900

    First code

---
+++ denops/ddc/sources/ultisnips.ts
@@ -0,0 +1,30 @@
+import {
+  BaseSource,
+  Candidate,
+  Context,
+  DdcOptions,
+  SourceOptions,
+} from "https://deno.land/x/ddc_vim@v0.0.13/types.ts";
+import { Denops, vars } from "https://deno.land/x/ddc_vim@v0.0.13/deps.ts";
+
+export class Source extends BaseSource {
+  async gatherCandidates(
+    denops: Denops,
+    _context: Context,
+    _ddcOptions: DdcOptions,
+    _options: SourceOptions,
+    _params: Record<string, unknown>,
+  ): Promise<Candidate[]> {
+    if (await vars.g.get(denops, "did_plugin_ultisnips") == null) {
+      return [];
+    }
+
+    const snippets = await denops.call("UltiSnips#SnippetsInCurrentScope") as {
+      [trigger: string]: string;
+    };
+    return Object.keys(snippets).map((trigger) => ({
+      word: trigger,
+      menu: snippets[trigger],
+    }));
+  }
+}
