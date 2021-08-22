3edd035f9ada3bbbc26e97b245d570dff66af78e
matsui54 <haru.matu9168@gmail.com>
2021-08-22 22:45:14 +0900

    update for the latest ddc

--- denops/ddc/sources/ultisnips.ts
+++ denops/ddc/sources/ultisnips.ts
@@ -1,25 +1,20 @@
 import {
   BaseSource,
   Candidate,
-  Context,
-  DdcOptions,
-  SourceOptions,
-} from "https://deno.land/x/ddc_vim@v0.1.0/types.ts";
-import { Denops, vars } from "https://deno.land/x/ddc_vim@v0.1.0/deps.ts";
+} from "https://deno.land/x/ddc_vim@v0.2.2/types.ts#^";
+import { Denops, vars } from "https://deno.land/x/ddc_vim@v0.2.2/deps.ts#^";
 
 export class Source extends BaseSource {
-  async gatherCandidates(
-    denops: Denops,
-    _context: Context,
-    _ddcOptions: DdcOptions,
-    _options: SourceOptions,
-    _params: Record<string, unknown>,
-  ): Promise<Candidate[]> {
-    if (await vars.g.get(denops, "did_plugin_ultisnips") == null) {
+  async gatherCandidates(args: {
+    denops: Denops;
+  }): Promise<Candidate[]> {
+    if (await vars.g.get(args.denops, "did_plugin_ultisnips") == null) {
       return [];
     }
 
-    const snippets = await denops.call("UltiSnips#SnippetsInCurrentScope") as {
+    const snippets = await args.denops.call(
+      "UltiSnips#SnippetsInCurrentScope",
+    ) as {
       [trigger: string]: string;
     };
     return Object.keys(snippets).map((trigger) => ({
